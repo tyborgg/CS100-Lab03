01e824a98260b4d1ee3ee8563b49cad29923019f
amiddendorp22 <amiddendorp22@gmail.com>
2020-10-25 18:33:34 -0700

    Added new (broken) main.cpp.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,5 @@
-#include <iostream>
-#include <string>
-
-int main()
+#include "c-echo.h"
+int main(int argv, char** argc)
 {
-    std::string name;
-    std::cout << "What is your name?" << std::endl;
-    std::cin >> name;
-    std::cout << "Hello " << name << "!" << std::endl;
-
-    return 0;
+	std::cout <<"Count of output: " << count(echo(argv,argc)) << std::endl;
 }
